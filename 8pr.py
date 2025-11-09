ee06154d9bc25e95d1127fecfacff374637e4761
aslpshh <fck7502@gmail.com>
2025-11-09 23:44:06 +0300

    Add files via upload

---
+++ 8pr.py
@@ -0,0 +1,24 @@
+n = int(input("Введите кол-во строк n: "))
+m = int(input("Введите колво столбцов m: "))
+A = []
+for i in range(n):
+    row = []
+    for j in range(m):
+        row.append(int(input(f"Введите элемент [{i},{j}]: ")))
+    A.append(row)
+print("Исходная матрица:")
+for row in A:
+    print(' '.join(map(str, row)))
+for i in range(n):
+    min_elem = min(A[i])
+    if min_elem % 2 == 0:
+        replacement = 0
+    else:
+        replacement = 1
+    for j in range(m):
+        if A[i][j] == min_elem:
+            A[i][j] = replacement
+            break  
+print("Матрица после замены минимальных элементов:")
+for row in A:
+    print(' '.join(map(str, row)))
