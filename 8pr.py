c059b559c1c1639b271b590c8be12a2dbb0fdfcf
aslpshh <fck7502@gmail.com>
2025-11-09 23:50:16 +0300

    Update 8pr.py

--- 8pr.py
+++ 8pr.py
@@ -1,3 +1,4 @@
+Задание 2
 n = int(input("Введите кол-во строк n: "))
 m = int(input("Введите колво столбцов m: "))
 A = []
@@ -20,5 +21,24 @@ for i in range(n):
             A[i][j] = replacement
             break  
 print("Матрица после замены минимальных элементов:")
+for row in A:
+
+    print(' '.join(map(str, row)))
+
+Задание 1
+n = int(input("Введите кол-во строк n: "))
+m = int(input("Введите кол-во столбцов m: "))
+A = []
+for i in range(n):
+    row = []
+    for j in range(m):
+        row.append(int(input(f"Введите элемент [{i},{j}]: ")))
+    A.append(row)
+print("Исходная матрица:")
+for row in A:
+    print(' '.join(map(str, row)))
+for i in range(n):
+    A[i].sort()
+print("Матрица после сортировки строк:")
 for row in A:
     print(' '.join(map(str, row)))
